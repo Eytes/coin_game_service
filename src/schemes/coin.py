80d46de1fb3ec334d4ecd05a3a2e2159ae952baa
Eytes <yura.shamanov2000@bk.ru>
2024-05-02 18:09:37 +0300

    Исправлена ошибка: каждый раз, запрашивая значение side у монетки, оно менялось, а должно быть статичным

--- src/schemes/coin.py
+++ src/schemes/coin.py
@@ -1,9 +1,16 @@
 from datetime import datetime, UTC
 from enum import Enum
-from uuid import uuid4, UUID
+from uuid import uuid4
 
 from Crypto.Random import random
-from pydantic import BaseModel, Field, computed_field, model_validator
+from pydantic import (
+    BaseModel,
+    Field,
+    model_validator,
+    StrictBool,
+    UUID4,
+    PositiveInt,
+)
 from typing_extensions import Self
 
 from .utils import get_hash_sha256
@@ -15,21 +22,22 @@ class CoinSide(str, Enum):
 
 
 class CoinHash(BaseModel):
-    id: UUID
+    id: UUID4
     hash: str
-    is_live: bool
+    is_live: StrictBool
 
 
 class Coin(BaseModel):
-    id: UUID = Field(alias="_id", default_factory=uuid4)
-    timestamp: int = Field(default_factory=lambda: int(datetime.now(UTC).timestamp()))
+    id: UUID4 = Field(alias="_id", default_factory=uuid4)
+    timestamp: PositiveInt = Field(
+        default_factory=lambda: int(datetime.now(UTC).timestamp())
+    )
     hash: str = ""
     salt: str = ""
-    is_live: bool = True
-
-    @computed_field
-    def side(self) -> CoinSide:
-        return random.choice([CoinSide.TAIL, CoinSide.HEAD])
+    is_live: StrictBool = True
+    side: CoinSide = Field(
+        default_factory=lambda: random.choice([CoinSide.TAIL, CoinSide.HEAD])
+    )
 
     @model_validator(mode="after")
     def calculate_hash_and_salt(self) -> Self:
